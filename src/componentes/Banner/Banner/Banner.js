8be72bcb22bb5df4a658ca06e5145a8e257b0e41
Rafi Aiub <rafi@selettra.com.br>
2023-06-27 10:50:34 -0300

    feat: Add index.js banner

---
+++ src/componentes/Banner/Banner/Banner.js
@@ -0,0 +1,9 @@
+import "./Banner.css";
+export const Banner = () => {
+  // JSX
+  return (
+    <header className="banner">
+      <img scr="./imagens/rodapé_selettra_2.png" alt="banner" />;
+    </header>
+  );
+};
