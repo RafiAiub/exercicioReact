3b0c237626db4d0946f973b3a78056a7c4546858
Rafi Aiub <rafi@selettra.com.br>
2023-06-27 10:35:29 -0300

    feat: Add base project file

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import Banner from "./componentes/Banner/Banner";
+import Formulario from "./componentes/Banner/Formulario";
+import Time from "./componentes/Banner/Time";
+
+function App() {
+
+  const times = [
+    {
+      nome: 'Administrativo',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Automação',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Back-End',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'Comercial',
+      corPrimaria: '#E06B89',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'Compras',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome: 'Diretoria',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Fabrica Montagem',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Financeiro',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    { 
+      nome: 'Montagem Eletrica',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Montagem Mecânica',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'Operações e Assistencia Técnica',
+      corPrimaria: '#E06B89',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'P&D',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome: 'Planejamento',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'RH',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Segurança do Trabalho',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'TI',
+      corPrimaria: '#E06B89',
+      corSecundaria: '#FDE7E8'
+    }
+  ]
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador])
+  }
+
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+
+      {times.map(time => <Time 
+      key={time.nome} 
+      nome={time.nome} 
+      corPrimaria={time.corPrimaria} 
+      corSecundaria={time.corSecundaria}
+      colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}
+
+
+    </div>
+  );
+}
+
+export default App;
