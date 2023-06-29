8ed59901b498ee23d857a27e594a3f2716a96b14
Rafi Aiub <rafi@selettra.com.br>
2023-06-29 11:01:08 -0300

    staged changes

--- src/componentes/Banner/Banner/Banner.js
+++
@@ -1,9 +0,0 @@
-import "./Banner.css";
-export const Banner = () => {
-  // JSX
-  return (
-    <header className="banner">
-      <img scr="./imagens/rodapé_selettra_2.png" alt="banner" />;
-    </header>
-  );
-};
