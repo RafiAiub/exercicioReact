691e3379c17810804b34e401e50e52a36cd1d0ea
Rafi Aiub <rafi@selettra.com.br>
2023-06-29 10:53:30 -0300

    feat: Add function to image

--- src/componentes/Banner/Banner/index.js
+++ src/componentes/Banner/Banner/index.js
@@ -1,3 +1,12 @@
-import { Banner } from "./Banner";
+import './Banner.css';
+import BannerImage from '../imagens/Banner.png';
 
-export default Banner;
+const BannerComponent = () => {
+  return (
+    <div>
+      <img src={BannerImage} alt="Logo" className='Banner'/>
+    </div>
+  );
+};
+
+export default BannerComponent;
